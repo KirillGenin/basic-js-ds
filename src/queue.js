f54bd50676ba7d8a44fccf84ba904945ea33ef3d
KirillGenin <332211op@gmail.com>
2022-10-18 02:18:21 +0300

    feat: add solution task Queue

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,21 +13,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+
 class Queue {
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+
+    this.head = null
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+
+    return this.head
+  }
+
+  enqueue(value) {
+
+    if (!this.head) {
+      this.head = new ListNode(value)
+      return
+    }
+
+    let lastInQueue = this.head
+
+    while (lastInQueue.next) lastInQueue = lastInQueue.next
+
+    lastInQueue.next = new ListNode(value)
+
+    return
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+    if (!this.head) return null
+
+    let firstInQueue = this.head.value
+    this.head = this.head.next
+    return firstInQueue
   }
 }
 
